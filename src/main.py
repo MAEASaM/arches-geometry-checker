3718eeda31bb77c3d7ab0ba48f283b080a11da12
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-12-09 06:50:05 +0000

    add base parser

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
 import pathlib
 from qgis.core import QgsProcessing
+import argparse
+
+Parser = argparse.ArgumentParser(description='Check geometry validity for Arches')
+
+Parser.add_argument('--input', '-i', dest='input', required=True, help='Input file')
+
+args = Parser.parse_args()
+inputfile_name = pathlib.Path(args.input).name
+output = pathlib.Path(args.input).parent / (str(inputfile_name.stem) + '_valid' + str(inputfile_name.suffix))
+
 
 
 
